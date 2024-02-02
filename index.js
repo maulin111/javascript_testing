4751769c3c47e7d9b4f32572ad087dd34b5f10b0
Maulin Desai <Maulin.Desai@einfochips.com>
2024-02-02 10:44:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+       // declare variable using let keyword
+       let name1 = 'Max';
+       console.log(name1);
+
+       //cannot be a reserved keyword
+       //should be meaningful
+       //cannot start with number
+       //cannot contains a space or hypen
+       //Are case-sensitive
+
+    // declare constant value using const keyword
+       const interestRate = 0.3
+    //    interestRate = 1
+       console.log(interestRate)
+
+       let name = 'Maulin Desai';
+       let age = 30;
+       let isApproved = false;
+       let firstName = undefined;
+    //    let selectedColor = null;
+
+       let person = {
+        name: 'Maulin Desai',
+        age: 30
+       };
+
+       //Dot Notation
+       person.name = 'Max Desai';
+
+       //Bracket Notation
+       let selection = 'name';
+       person[selection]='Mary';
+
+       console.log(person[selection])
+
+       //how to declare and access Arrays
+       let selectedColor = ['red','blue'];
+       selectedColor[2] = 'green';
+       console.log(selectedColor);
+
+       //how to declare function and access the arguments - Performing a task
+       function greet(name, lastName){
+        console.log('Hello ' +name + " " +lastName);     
+       }
+       greet('john','smith');
+
+       //function calculate value
+       function calculate(a,b) {
+           return a + b;
+       }
+       let result = calculate(13,12);
+       console.log(result);
+
+       //Arithmetic operators
+       let x = 4;
+       let y = 3;
+
+       console.log(x + y);
+       console.log(x - y);
+       console.log(x * y);
+       console.log(x / y);
+       console.log(x % y)
+       console.log(x ** y);
+       console.log(x++);
+       console.log(x);
+       console.log(--x);
+
+       //Ternary operators
+       let points = 110;
+       let type = points > 100 ? 'Gold' : 'silver';
+       console.log(type)
+       
+
